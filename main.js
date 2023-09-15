720c4f064ac58790112b795ddc6928bb3eee9288
Kauany12389 <106532365+Kauany12389@users.noreply.github.com>
2023-09-15 10:58:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+    }
+    document.querySelectorAll('.tecla');
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+    listaDeTeclas[0].onclick = tocaSomPom;
+    
+    
