08dfd753f6e50b82a96863c787363ef5caa74261
Kauany12389 <106532365+Kauany12389@users.noreply.github.com>
2023-09-22 11:00:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
     }
-    document.querySelectorAll('.tecla');
+    document.querySelectorAll('.tecla');while(contador < listaDeTeclas.length) 
     const listaDeTeclas = document.querySelectorAll('.tecla');
-    listaDeTeclas[0].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    let contador = 0;
+    // Estrutura de repetição - Enquanto
+    while(contador <8) 
+    while(contador <9) 
+    while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador= contador + 1;
+    console.log(contador);
+        }
     
     
